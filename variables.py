bf42c8284bee9cf6bec0345b7d97d4d72abd4009
Tommy Cirilo-Thorpe <tommy.v.cirilo@gmail.com>
2021-04-21 17:12:52 -0500

    completed the different types of variables file

---
+++ variables.py
@@ -0,0 +1,41 @@
+#A variable is a container for a value, which can be of various types
+
+'''
+This is a multiline comment
+or docstring (used to define a functions purpose)
+can be single or double quotes
+'''
+
+"""
+Variable Rules:
+- variable names are case sensitive (name and NAME are different variables)
+- Must start with a a letter or an underscore
+- Can have numbers but can not start with one
+"""
+
+x = 1       # int
+y = 2.5     # float
+name = 'John'   # string
+is_cool = True  # boolean
+
+#Multiple assignment
+b, c, last, fun = (2, 3.5, 'Smith', False)
+
+print(b, c, last, fun)
+
+# in order to run a file you tupe python3 and the file name
+
+# Basic Math
+g = b + c
+
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+
+print (type(z), z)
+
+
+
