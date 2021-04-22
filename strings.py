72f1368e782c1e5fbe6a001e8cf5cba075e81d49
Tommy Cirilo-Thorpe <tommy.v.cirilo@gmail.com>
2021-04-21 19:49:50 -0500

    added more string manipulation examples

--- strings.py
+++ strings.py
@@ -14,3 +14,30 @@ print('My name is {name} and I am {age}'.format(name=name, age=age))
 # F=Strings (3.6+)
 print(f'Hello, my name is {name} and I am {age}')
 
+# String Methods
+
+s = 'hello world'
+
+# Capitalize first word of string
+print(s.capitalize())
+
+# Make all uppercase
+print(s.upper())
+
+# Make all lowercase
+print(s.lower())
+
+# Swap Case
+print(s.swapcase())
+
+# Get lenth of string
+print(len(s))
+
+# Replace
+print(s.replace('world', 'everyone'))
+
+# Count
+sub = 'h'
+print(s.count(sub))
+
+
